d92db0a8da4771f973eff9cf2973501a932694af
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-14 14:49:49 +0000

    feat: add validation middleware for login

---
+++ apps/backend/src/middlewares/login_validate.middleware.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Response } from "express";
+import { LoginRequest } from "../types/requests/auth.request.type";
+import { loginSchema } from "../schemas/auth.schema";
+import { ApiError } from "../lib/api_error";
+import http_status_codes from "../config/http_status_codes";
+
+
+const login_validate = (req: LoginRequest, _res:Response, next: NextFunction) => {
+  const validate = loginSchema.safeParse(req.body);
+
+  if (!validate.success) {
+    const wrongTypeError = new ApiError(
+      "Server received data with wrong type",
+      http_status_codes.BAD_REQUEST
+    );
+
+    next(wrongTypeError);
+    return;
+  }
+
+  next();
+};
+
+export default login_validate;
