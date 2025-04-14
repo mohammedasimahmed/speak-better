52522b8a51806764c77d6e9c86aef53674cee75a
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-14 12:52:26 +0000

    feat: add register request type

--- apps/backend/src/types/index.d.ts
+++ apps/backend/src/types/index.d.ts
@@ -2,3 +2,9 @@ export interface LoginRequestBody {
     username: string;
     password: string;
 }
+
+export interface RegisterRequestBody {
+    username: string;
+    email: string;
+    password: string;
+}
--- apps/backend/src/types/requests/auth.request.type.ts
+++ apps/backend/src/types/requests/auth.request.type.ts
@@ -1,5 +1,9 @@
-import { LoginRequestBody } from "..";
+import { LoginRequestBody, RegisterRequestBody } from "..";
 
 export interface LoginRequest {
     body: LoginRequestBody
+}
+
+export interface RegisterRequest {
+    body: RegisterRequestBody
 }
