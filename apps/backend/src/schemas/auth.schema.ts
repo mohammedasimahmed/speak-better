9f395c73d8fc1c7bef5ddd7add8bdb37951f3603
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-14 13:51:33 +0000

    feat: add register schema for validation

--- apps/backend/src/schemas/auth.schema.ts
+++ apps/backend/src/schemas/auth.schema.ts
@@ -4,3 +4,9 @@ export const loginSchema = z.object({
   username: z.string(),
   password: z.string()
 });
+
+export const registerSchema = z.object({
+  username: z.string(),
+  email: z.string().email(),
+  password: z.string()
+});
