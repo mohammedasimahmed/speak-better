b677098fc4356199e95cf2608a7642e93f834fd6
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-14 10:51:18 +0000

    feat: add cors configuration

--- apps/backend/src/config/config.ts
+++ apps/backend/src/config/config.ts
@@ -3,9 +3,11 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT || 5000,
-  FRONTEND_URL: process.env.FRONTEND_URL,
-  DATABASE_URL: process.env.DATABASE_URL,
-  ENVIRONMENT: process.env.ENVIRONMENT
+  FRONTEND_URLS: process.env.FRONTEND_URL
+    ? [process.env.FRONTEND_URL, "http://localhost:3000"]
+    : ["http://localhost:3000"],
+  DATABASE_URL: process.env.DATABASE_URL!,
+  ENVIRONMENT: process.env.ENVIRONMENT!
 };
 
 export default config;
--- apps/backend/src/loaders/express.loader.ts
+++ apps/backend/src/loaders/express.loader.ts
@@ -1,12 +1,20 @@
 import express, { Application } from "express";
 import config from "../config/config";
+import cors from "cors";
 
 const expressLoader = (app: Application) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
-  if(config.ENVIRONMENT === "development") {
-    import ("morgan").then(morgan => {
+  app.use(
+    cors({
+      origin: config.FRONTEND_URLS,
+      credentials: true
+    })
+  );
+
+  if (config.ENVIRONMENT === "development") {
+    import("morgan").then(morgan => {
       app.use(morgan.default("dev"));
     }
     );
