b232f4965bd8e44fea5bf5584dd39d7a1d564612
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-15 18:16:39 +0000

    feat: add improve speech type validation middleware

---
+++ apps/backend/src/middlewares/improve_speech_validate.middleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Response } from "express";
+import { ImproveSpeechRequest } from "../types/requests/improve_speech.request.type";
+import { improveSpeechSchema } from "../schemas/improve_speech.schema";
+import { ApiError } from "../lib/api_error";
+import http_status_codes from "../config/http_status_codes";
+
+
+const improve_speech_validate = (req: ImproveSpeechRequest, _res: Response, next: NextFunction) => {
+  const validate = improveSpeechSchema.safeParse(req.body);
+  if (!validate.success) {
+    const wrongTypeError = new ApiError(
+      "Server received data with wrong type",
+      http_status_codes.BAD_REQUEST
+    );
+
+    next(wrongTypeError);
+    return;
+  }
+
+  next();
+};
+
+export default improve_speech_validate;
