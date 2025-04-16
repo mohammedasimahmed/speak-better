4c5cb4ac418f29afddbac16eb343ca67dc80f1b0
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-16 10:50:38 +0000

    feat: add speech route
     - added speech route for speech improvements

--- apps/backend/src/loaders/routes.loader.ts
+++ apps/backend/src/loaders/routes.loader.ts
@@ -1,6 +1,8 @@
 import { Express } from "express";
 import authRoutes from "../routes/auth.routes";
+import speechRoute from "../routes/improve_speech.route";
 
 export default function routesLoader(app: Express) {
   app.use("/api/auth", authRoutes);
+  app.use("/api", speechRoute);
 }
---
+++ apps/backend/src/routes/improve_speech.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import improveSpeechController from "../controllers/improve_speech.controller";
+import improve_speech_validate from "../middlewares/validate/improve_speech.validate.middleware";
+
+const router = Router();
+
+router.post("/speech", improve_speech_validate, improveSpeechController);
+
+export default router;
