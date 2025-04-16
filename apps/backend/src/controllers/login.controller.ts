7b5869c9420aea9f960f612f2e4245fd02e33b5b
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-16 11:45:07 +0000

    fix: move refresh token to cookie in login response

--- apps/backend/src/controllers/login.controller.ts
+++ apps/backend/src/controllers/login.controller.ts
@@ -1,18 +1,25 @@
 import { Request, Response, NextFunction } from "express";
 import http_status_codes from "../config/http_status_codes";
 import loginUserService from "../services/login.service";
+import config from "../config/config";
 
 const loginUserController = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const user = req.body;
 
-    const { accessToken, refreshToken, existingUser} = await loginUserService(user);
+    const { accessToken, refreshToken, existingUser } = await loginUserService(user);
+
+    res.cookie("refresh", refreshToken, {
+      httpOnly: true,
+      secure: config.ENVIRONMENT === "production",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
 
     res.status(http_status_codes.OK).json({
       message: "Login successful",
       accessToken,
-      refreshToken,
-      user:existingUser
+      user: existingUser
     });
   } catch (error) {
     next(error);
