de5fe735ec23f851f8fe681bc8b9319b33f520ff
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-17 13:45:38 +0000

    fix: fix error handling issue
     - fix error not being passed to global error
       handler

--- apps/backend/src/loaders/index.ts
+++ apps/backend/src/loaders/index.ts
@@ -5,8 +5,8 @@ import routesLoader from "./routes.loader";
 
 const loader_intializer = (app: Express) => {
   expressLoader(app);
-  errorHandlerLoader(app);
   routesLoader(app);
+  errorHandlerLoader(app);
 };
 
 export default loader_intializer;
