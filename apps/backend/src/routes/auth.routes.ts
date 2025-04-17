98074ffca054b61c9733fdbc51742fcc43f1b736
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-17 17:11:01 +0000

    chore: change name of imported refreshController

--- apps/backend/src/routes/auth.routes.ts
+++ apps/backend/src/routes/auth.routes.ts
@@ -3,12 +3,12 @@ import registerUserController from "../controllers/register.controller";
 import register_validate from "../middlewares/validate/register.middleware";
 import loginUserController from "../controllers/login.controller";
 import login_validate from "../middlewares/validate/login.middleware";
-import refresh from "../controllers/refresh.controller";
+import refreshController from "../controllers/refresh.controller";
 
 const router = Router();
 
 router.post("/login", login_validate, loginUserController);
 router.post("/register", register_validate, registerUserController);
-router.get("/refresh", refresh);
+router.get("/refresh", refreshController);
 
 export default router;
