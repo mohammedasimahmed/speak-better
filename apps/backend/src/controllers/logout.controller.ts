3184d77fc9f8e511b377276793c6aaa6cc08bb90
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-17 17:32:59 +0000

    feat: add logout functionality at backend

---
+++ apps/backend/src/controllers/logout.controller.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import { ApiError } from "../lib/api_error";
+import http_status_codes from "../config/http_status_codes";
+import config from "../config/config";
+
+const logoutController = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.cookies) {
+    const cookiesMissing = new ApiError(
+      "No cookies Found",
+      http_status_codes.BAD_REQUEST
+    );
+
+    next(cookiesMissing);
+    return;
+  }
+
+  const {cookies} = req;
+  if (!cookies.refresh) {
+    const refreshTokenMissing = new ApiError(
+      "Refresh Token is missing in the cookies",
+      http_status_codes.BAD_REQUEST
+    );
+
+    next(refreshTokenMissing);
+    return;
+  }
+
+  res.clearCookie("refresh", {
+    httpOnly: true,
+    secure: config.ENVIRONMENT === "production",
+    sameSite: "strict",
+    maxAge: 0,
+  });
+
+  res.status(http_status_codes.OK).json({ message: "Logged out successfully" });
+};
+
+export default logoutController;
--- apps/backend/src/routes/auth.routes.ts
+++ apps/backend/src/routes/auth.routes.ts
@@ -4,11 +4,13 @@ import register_validate from "../middlewares/validate/register.middleware";
 import loginUserController from "../controllers/login.controller";
 import login_validate from "../middlewares/validate/login.middleware";
 import refreshController from "../controllers/refresh.controller";
+import logoutController from "../controllers/logout.controller";
 
 const router = Router();
 
 router.post("/login", login_validate, loginUserController);
 router.post("/register", register_validate, registerUserController);
 router.get("/refresh", refreshController);
+router.get("/logout", logoutController);
 
 export default router;
