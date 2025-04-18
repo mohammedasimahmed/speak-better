4c66b290295ae24e404488fd622eb165fbbe4b75
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-18 08:08:44 +0000

    feat: add userAtom

---
+++ apps/frontend/src/store/index.ts
@@ -0,0 +1,9 @@
+import { atom } from "jotai";
+
+export type userType = {
+    id: string,
+    username: string,
+    email: string,
+    password: string
+}
+export const userAtom = atom<userType>();
