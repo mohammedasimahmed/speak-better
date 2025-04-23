42471ee6e96e2fe35722380322619ea92d0f0b26
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-23 10:20:32 +0000

    chore: improve verify token error handling

--- apps/backend/src/middlewares/verify_token.middleware.ts
+++ apps/backend/src/middlewares/verify_token.middleware.ts
@@ -1,9 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { ApiError } from "../lib/api_error";
 import http_status_codes from "../config/http_status_codes";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt, { JwtPayload, JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 import config from "../config/config";
-
 
 const verifyToken = (req: Request, _res: Response, next: NextFunction) => {
   if (!req.headers) {
@@ -26,7 +25,7 @@ const verifyToken = (req: Request, _res: Response, next: NextFunction) => {
     return;
   }
 
-  const [,accessToken] = req.headers["authorization"].split(" ");
+  const [, accessToken] = req.headers["authorization"].split(" ");
 
   if (!accessToken) {
     const accessTokenMissing = new ApiError(
@@ -42,7 +41,19 @@ const verifyToken = (req: Request, _res: Response, next: NextFunction) => {
     jwt.verify(accessToken, config.JWT_ACCESS_TOKEN_SECRET) as JwtPayload;
     next();
   } catch (error) {
-    next(error);
+    if (error instanceof TokenExpiredError) {
+      next(new ApiError("Access token has expired", http_status_codes.UNAUTHORIZED));
+      return;
+    }
+
+    if (error instanceof JsonWebTokenError) {
+      next(new ApiError("Invalid access token", http_status_codes.UNAUTHORIZED));
+      return;
+    }
+
+    // Generic fallback
+    next(new ApiError("Unexpected Error", http_status_codes.INTERNAL_SERVER_ERROR));
+    return;
   }
 };
 
