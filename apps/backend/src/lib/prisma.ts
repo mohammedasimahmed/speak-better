f6854cf79697c3446606135d5d57cb94a7aa1f1e
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-29 12:50:50 +0000

    fix: use config.ENVIRONMENT in prisma.ts

--- apps/backend/src/lib/prisma.ts
+++ apps/backend/src/lib/prisma.ts
@@ -1,6 +1,6 @@
-// src/lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import config from "../config/config";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient & ReturnType<typeof PrismaClient.prototype.$extends>
@@ -10,4 +10,4 @@ export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient().$extends(withAccelerate());
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (config.ENVIRONMENT !== "production") globalForPrisma.prisma = prisma;
