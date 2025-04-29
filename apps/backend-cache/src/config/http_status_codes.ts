1a1c5da42a1119b92419a13dedb5653cc0acd667
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-29 06:28:55 +0000

    feat: add http status codes in backend-cache

---
+++ apps/backend-cache/src/config/http_status_codes.ts
@@ -0,0 +1,36 @@
+const http_status_codes = {
+  // Success Codes
+  "OK": 200,
+  "CREATED": 201,
+  "ACCEPTED": 202,
+  "NO_CONTENT": 204,
+
+  // Redirection Codes
+  "MOVED_PERMANENTLY": 301,
+  "FOUND": 302,
+  "SEE_OTHER": 303,
+  "NOT_MODIFIED": 304,
+
+  // Client Error Codes
+  "BAD_REQUEST": 400,
+  "UNAUTHORIZED": 401,
+  "FORBIDDEN": 403,
+  "NOT_FOUND": 404,
+  "METHOD_NOT_ALLOWED": 405,
+  "NOT_ACCEPTABLE": 406,
+  "REQUEST_TIMEOUT": 408,
+  "CONFLICT": 409,
+  "GONE": 410,
+  "UNSUPPORTED_MEDIA_TYPE": 415,
+  "TOO_MANY_REQUESTS": 429,
+
+  // Server Error Codes
+  "INTERNAL_SERVER_ERROR": 500,
+  "NOT_IMPLEMENTED": 501,
+  "BAD_GATEWAY": 502,
+  "SERVICE_UNAVAILABLE": 503,
+  "GATEWAY_TIMEOUT": 504,
+};
+
+
+export default http_status_codes;
