256facb7f3c34d3b5b052662fc798f8126c1f686
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-29 16:27:10 +0000

    chore: change server startup log message

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -10,7 +10,7 @@ function startServer() {
   const {PORT} = config;
 
   app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on ${PORT}`);
   });
 }
 
