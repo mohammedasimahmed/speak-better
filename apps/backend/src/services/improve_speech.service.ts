be5dd96e1fc00f42bf49fb1267636394a9dfdc0e
mohammedasimahmed <mohammedasim9298@gmail.com>
2025-04-29 13:17:59 +0000

    fix: add error handling at improve speech functionality

--- apps/backend/src/services/improve_speech.service.ts
+++ apps/backend/src/services/improve_speech.service.ts
@@ -1,16 +1,22 @@
 import llm from "../lib/llm";
 import instruction from "../lib/instruction";
+import { ApiError } from "../lib/api_error";
+import http_status_codes from "../config/http_status_codes";
 
 const improveSpeechService = async (speech: string[], emotion: string[]) => {
-  const aiMsg = await llm.invoke([
-    [
-      "system",
-      instruction,
-    ],
-    ["human", `speech: ${JSON.stringify(speech)}, emotion: ${JSON.stringify(emotion)}`],
-  ]);
+  try {
+    const aiMsg = await llm.invoke([
+      [
+        "system",
+        instruction,
+      ],
+      ["human", `speech: ${JSON.stringify(speech)}, emotion: ${JSON.stringify(emotion)}`],
+    ]);
 
-  return aiMsg.content;
+    return aiMsg.content;
+  } catch {
+    throw new ApiError("Unexpected Error", http_status_codes.INTERNAL_SERVER_ERROR);
+  }
 };
 
 export default improveSpeechService;
